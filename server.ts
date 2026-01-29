ff96a02a3a5ba45ee6534bf74c8a384ee7fc9585
George Kiknadze <g.kiknadze@hotmail.com>
2026-01-29 12:19:10 +0400

    Move index.html to root for GitHub Pages

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ const server = Bun.serve({
     const url = new URL(req.url);
     let path = url.pathname === "/" ? "/index.html" : url.pathname;
 
-    const file = Bun.file(`./src${path}`);
+    const file = Bun.file(`.${path}`);
 
     if (await file.exists()) {
       return new Response(file);
